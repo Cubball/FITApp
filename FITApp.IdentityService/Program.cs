4434788430fbe0f2ee63d2957e5bc24ea84c4386
Cubball <138378823+Cubball@users.noreply.github.com>
2024-03-07 18:28:02 +0200

    Register DbContext and add initial migration

--- FITApp.IdentityService/Program.cs
+++ FITApp.IdentityService/Program.cs
@@ -1,10 +1,18 @@
+using FITApp.IdentityService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// NOTE: for now, we only have an SQLite database for development, we'll add another one later
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=app.db"));
+
 var app = builder.Build();
+
+// TODO: initialize the database with the first admin user
 
 if (app.Environment.IsDevelopment())
 {
