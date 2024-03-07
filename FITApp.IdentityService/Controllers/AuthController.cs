e6a5beecc7096162c1499471d017164edbafbc30
Cubball <138378823+Cubball@users.noreply.github.com>
2024-03-07 20:47:56 +0200

    Implement login and refresh endpoints

--- FITApp.IdentityService/Controllers/AuthController.cs
+++ FITApp.IdentityService/Controllers/AuthController.cs
@@ -1,7 +1,8 @@
 using FITApp.IdentityService.Contracts.Requests;
 using FITApp.IdentityService.Contracts.Responses;
-
+using FITApp.IdentityService.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace FITApp.IdentityService.Controllers;
 
@@ -9,18 +10,67 @@ namespace FITApp.IdentityService.Controllers;
 [Route("api/[controller]")]
 public class AuthController : ControllerBase
 {
-    [HttpPost("login")]
-    public ActionResult<TokensResponse> Login([FromBody] LoginRequest request)
+    private readonly ITokenService _tokenService;
+    private readonly IUserService _userService;
+
+    public AuthController(ITokenService tokenService, IUserService userService)
     {
-        // TODO:
-        throw new NotImplementedException();
+        _tokenService = tokenService;
+        _userService = userService;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<TokensResponse>> Login([FromBody] LoginRequest request)
+    {
+        var credentialsValid = await _userService.IsUserValidAsync(request.Email, request.Password);
+        if (!credentialsValid)
+        {
+            return Unauthorized();
+        }
+
+        var jwt = _tokenService.GenerateJwt(await _userService.GetClaimsAsync(request.Email));
+        var refreshToken = _tokenService.GenerateRefreshToken();
+        var success = await _userService.UpdateRefreshTokenAsync(request.Email, refreshToken);
+        if (!success)
+        {
+            return StatusCode(500);
+        }
+
+        return new TokensResponse
+        {
+            AccessToken = jwt,
+            RefreshToken = refreshToken,
+        };
     }
 
     [HttpPost("refresh")]
-    public ActionResult<TokensResponse> Refresh([FromBody] RefreshRequest request)
+    public async Task<ActionResult<TokensResponse>> Refresh([FromBody] RefreshRequest request)
     {
-        // TODO:
-        throw new NotImplementedException();
+        var (claims, isValid) = await _tokenService.GetClaimsFromExpiredTokenAsync(request.AccessToken);
+        if (!isValid)
+        {
+            return Unauthorized();
+        }
+
+        var email = claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Email)?.Value;
+        if (email is null)
+        {
+            return Unauthorized();
+        }
+
+        var jwt = _tokenService.GenerateJwt(claims);
+        var refreshToken = _tokenService.GenerateRefreshToken();
+        var success = await _userService.UpdateRefreshTokenAsync(email, refreshToken);
+        if (!success)
+        {
+            return StatusCode(500);
+        }
+
+        return new TokensResponse
+        {
+            AccessToken = jwt,
+            RefreshToken = refreshToken,
+        };
     }
 
     [HttpPost("change-password")]
