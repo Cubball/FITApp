88ce822446f305350404718c405176160ac532d9
Cubball <138378823+Cubball@users.noreply.github.com>
2024-03-07 20:44:08 +0200

    Add IUserService

---
+++ FITApp.IdentityService/Services/IUserService.cs
@@ -0,0 +1,14 @@
+using System.Security.Claims;
+
+namespace FITApp.IdentityService.Services;
+
+public interface IUserService
+{
+    Task<bool> IsUserValidAsync(string email, string password);
+
+    Task<bool> TryChangePasswordAsync(string email, string oldPassword, string newPassword);
+
+    Task<IEnumerable<Claim>> GetClaimsAsync(string email);
+
+    Task<bool> UpdateRefreshTokenAsync(string email, string refreshToken);
+}
