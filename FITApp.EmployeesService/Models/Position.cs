4de34a70be62923d36e1bb721acb127f9d966e2e
Vlad Danilchuk <vladdanilchuk9@gmail.com>
2024-03-19 18:09:24 +0200

    add models

---
+++ FITApp.EmployeesService/Models/Position.cs
@@ -0,0 +1,11 @@
+namespace FITApp.EmployeesService.Models
+{
+    public class Position
+    {
+        public string Name { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+
+    }
+
+}
