41204cd53fe7d78eda352d432ce0cb5602fbc40a
Daria Kravchuk <daria1kravchuk@gmail.com>
2024-04-19 16:05:19 +0300

    Update method signatures in interfaces to reflect changes in parameter types or return types

--- FITApp.EmployeesService/Interfaces/IEmployeesRepository.cs
+++ FITApp.EmployeesService/Interfaces/IEmployeesRepository.cs
@@ -1,6 +1,6 @@
 using System.Linq.Expressions;
-using FITApp.EmployeesService.Dtos;
 using FITApp.EmployeesService.Models;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace FITApp.EmployeesService.Interfaces
@@ -13,13 +13,19 @@ namespace FITApp.EmployeesService.Interfaces
         Task<UpdateResult> UpdateEmployee(string id, UpdateDefinition<Employee> update);
         Task<DeleteResult> DeleteEmployee(string id);
         Task<List<AcademicRank>> GetAcademicRanksByEmployeeId(string id);
-        Task<UpdateResult> RemoveArrayElementByIndex<TElement>(
-                                                                string id,
+        Task<UpdateResult> RemoveArrayElementByIndex<TElement>(string id,
                                                                 int index,
                                                                 Func<Employee, List<TElement>> selector,
                                                                 Expression<Func<Employee, object>> expression);
-        Task<long> TotalCounDocument();
-        List<Employee> GetEmployeesByPage(int page, int pageSize);
+        Task<long> TotalCountDocuments(FilterDefinition<Employee> filter);
+        
+        // Task<IEnumerable<Employee>> GetEmployeesByPage(FilterDefinition<Employee> filter, int page, int pageSize);
+        // Task<IEnumerable<SimpleEmployeeDto>> GetEmployeesByPage2(FilterDefinition<Employee> filter, int page, int pageSize);
 
+        // Task<List<BsonDocument>> GetEmployeesByPage3(FilterDefinition<Employee> filter,
+        //     IProjection<Employee> projection, int page, int pageSize);
+
+        Task<IEnumerable<BsonDocument>> GetEmployeesByPage(FilterDefinition<Employee> filter,
+            ProjectionDefinition<Employee> projection, int page, int pageSize);
     }
 }
