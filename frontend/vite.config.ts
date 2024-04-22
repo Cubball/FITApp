0fc2277d9a38f9f1c0d7a7aa0e47d76ea99993fe
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-22 09:07:08 +0300

    Configure tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwind from 'tailwindcss'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -8,5 +9,10 @@ export default defineConfig(({ mode }) => {
       'process.env.BACKEND_URL': JSON.stringify(env.BACKEND_URL)
     },
     plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [tailwind()]
+      }
+    }
   }
 })
