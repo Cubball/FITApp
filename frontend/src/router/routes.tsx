9fb157a677389cf9d05cec4bec0093c61566c03d
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-23 13:00:11 +0300

    Add login route

--- frontend/src/router/routes.tsx
+++ frontend/src/router/routes.tsx
@@ -1,15 +1,25 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { RouteObject, createBrowserRouter } from 'react-router-dom';
+import LoginScreen from '../modules/auth/login.screen';
+
+const sharedRoutes: RouteObject[] = [
+  {
+    path: '/login',
+    element: <LoginScreen />
+  }
+];
 
 export const publicRoutes = createBrowserRouter([
   {
     path: '/',
     element: <div>Hello</div>
-  }
+  },
+  ...sharedRoutes
 ]);
 
 export const privateRoutes = createBrowserRouter([
   {
     path: '/',
     element: <div>Hello</div>
-  }
+  },
+  ...sharedRoutes
 ]);
