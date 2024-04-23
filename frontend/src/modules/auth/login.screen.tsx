9d6ce618d321266c259935b437d58b6f0494aded
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-23 12:57:07 +0300

    Add login screen

--- frontend/src/modules/auth/login.screen.tsx
+++ frontend/src/modules/auth/login.screen.tsx
@@ -1,6 +1,27 @@
+import { useAuth } from '../../shared/hooks/auth.hook';
+import { IAuthData } from '../../services/auth/auth.types';
+import LoginForm from './login.form';
 
 const LoginScreen = () => {
+  const { handleLogin, isLoginLoading } = useAuth();
+  const onFormSubmit = (values: IAuthData) => {
+    handleLogin(values);
+  };
+  return (
+    <div className="relative bg-gradient-to-r from-blue-200 font-comfortaa text-gray-800 md:overflow-hidden">
+      <div className="absolute left-1/2 top-[20%] z-[-1] hidden h-screen w-screen -rotate-45 bg-document bg-contain bg-no-repeat opacity-35 md:block"></div>
+      <div className="absolute -top-[80%] left-3/4 z-[-1] hidden h-screen w-screen -rotate-45 bg-document bg-contain bg-no-repeat opacity-65 md:block"></div>
+      <div className="absolute -top-[30%] left-[62.5%] z-[-1] hidden h-screen w-screen -rotate-45 bg-document bg-contain bg-no-repeat md:block"></div>
+      <div className="flex h-screen flex-col justify-start">
+        <h1 className="w-full pt-4 text-center text-5xl font-medium md:pl-6 md:text-left">
+          FITApp
+        </h1>
+        <div className="flex grow flex-col justify-center">
+          <LoginForm onSubmit={onFormSubmit} isLoginLoading={isLoginLoading}/>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default LoginScreen
+export default LoginScreen;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        comfortaa: ['"TT Firs Neue Trl"', 'sans-seris']
+      },
+      backgroundImage: {
+        document: 'url("./src/assets/document.svg")'
+      }
+    }
   },
   plugins: []
 };
