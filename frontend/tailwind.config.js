ffc410f752b101d8329519c4640b075a3f2774b4
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-23 17:57:38 +0300

    Add theme colors and icons for the sidebar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,12 @@ export default {
       },
       backgroundImage: {
         document: 'url("./src/assets/document.svg")'
+      },
+      colors: {
+        'main-background': '#BBDAF6',
+        'main-text': '#384D6C',
+        'link-active': '#CCE4FF',
+        'link-accent': '#acc9e3'
       }
     }
   },
