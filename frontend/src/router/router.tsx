1be913be0263f00e5e41dfbf31b8fe2b8837397c
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-23 13:04:40 +0300

    Return early in the router if refresh is not done yet, would need to change it later

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -3,7 +3,12 @@ import { privateRoutes, publicRoutes } from './routes';
 import { useAuth } from '../shared/hooks/auth.hook';
 
 const Router = () => {
-  const { authData } = useAuth();
+  const { authData, isRefreshLoading } = useAuth();
+
+  // TODO: handle loading properly
+  if (isRefreshLoading) {
+    return <div>Loading...</div>;
+  }
 
   return <RouterProvider router={authData?.accessToken ? privateRoutes : publicRoutes} />;
 };
