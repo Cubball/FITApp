676808c05fd8c60b4a14c7a6cb230c9aff910b0c
Cubball <138378823+Cubball@users.noreply.github.com>
2024-04-23 22:29:19 +0300

    Fix mixed up routes

--- frontend/src/layouts/main/sidebar.component.tsx
+++ frontend/src/layouts/main/sidebar.component.tsx
@@ -8,8 +8,8 @@ const Sidebar = () => {
     <aside className="sticky flex h-screen shrink grow basis-[20%] flex-col justify-between overflow-y-hidden bg-main-background py-10">
       <div>
         {/* TODO: determine routes accessible to the user and display corresponding SidebarItems */}
-        <SidebarItem icon={PublicationsIcon} text="Мої публікації" route="profile" />
-        <SidebarItem icon={ProfileIcon} text="Мій профіль" route="publications" />
+        <SidebarItem icon={PublicationsIcon} text="Мої публікації" route="publications" />
+        <SidebarItem icon={ProfileIcon} text="Мій профіль" route="profile" />
       </div>
       <LogoutButton />
     </aside>
