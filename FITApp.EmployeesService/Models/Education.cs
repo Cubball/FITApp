5cf0af5e4c9904c73123afdccaf3bfc7f43bc159
Cubball <138378823+Cubball@users.noreply.github.com>
2024-05-10 15:44:47 +0300

    Fix deviations from previously defined api schema

--- FITApp.EmployeesService/Models/Education.cs
+++ FITApp.EmployeesService/Models/Education.cs
@@ -3,7 +3,7 @@ namespace FITApp.EmployeesService.Models
     public class Education
     {
         public string University { get; set; }
-        public string Specialization { get; set; }
+        public string Specialty { get; set; }
         public DateOnly DiplomaDateOfIssue { get; set; }
     }
 }
