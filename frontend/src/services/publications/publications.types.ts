bc01f9fc966380e7d4a40a0c4d1b0fe1caffe856
Cubball <138378823+Cubball@users.noreply.github.com>
2024-05-17 21:39:20 +0300

    Add publications service and corresponding types

---
+++ frontend/src/services/publications/publications.types.ts
@@ -0,0 +1,47 @@
+export interface IPublicationShortInfo {
+  id: string;
+  type: string; // TODO: maybe enum
+  name: string;
+  dateOfPublication: string;
+}
+
+export interface IPublicationsPagedList {
+  page: number;
+  pageSize: number;
+  total: number;
+  publications: IPublicationShortInfo[];
+}
+
+export interface IPublication {
+  id: string;
+  name: string;
+  type: string;
+  annotation: string;
+  eVersionLink: string;
+  dateOfPublication: string;
+  author: IAuthor;
+  coauthors: ICoauthor[];
+}
+
+export interface ICreateUpdatePublication {
+  name: string;
+  type: string;
+  annotation: string;
+  eVersionLink: string;
+  dateOfPublication: string;
+  coauthors: ICoauthor[];
+}
+
+export interface IAuthor {
+  id: string;
+  firstName: string;
+  lastName: string;
+  patronymic: string;
+}
+
+export interface ICoauthor {
+  id?: string;
+  firstName: string;
+  lastName: string;
+  patronymic: string;
+}
