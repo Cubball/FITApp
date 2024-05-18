ca71c2b76d81422a40f8d3d42d91917c55b590c2
Cubball <138378823+Cubball@users.noreply.github.com>
2024-05-18 19:00:04 +0300

    Add a link to reports page in the publications-list component

--- frontend/src/modules/publications/publications-list.component.tsx
+++ frontend/src/modules/publications/publications-list.component.tsx
@@ -1,7 +1,8 @@
 import { NavLink, useLocation } from 'react-router-dom';
 import { usePublicationsList } from '../../shared/hooks/publications-list.hook';
 import Loading from '../../shared/components/loading';
-import PlusIcon from '../../assets/icons/plus-icon.svg'
+import PlusIcon from '../../assets/icons/plus-icon.svg';
+import ReportIcon from '../../assets/icons/report-icon.svg';
 import Error from '../../shared/components/error';
 import PublicationEntry from './publication-entry.component';
 import Pagination from '../../shared/components/pagination';
@@ -13,25 +14,35 @@ const PublicationsList = () => {
     Number(page ?? 1)
   );
   if (isLoading) {
-    return <Loading />
+    return <Loading />;
   }
 
   if (!publicationsList) {
-    return <Error />
+    return <Error />;
   }
 
   const totalPages = Math.ceil(publicationsList.total / publicationsList.pageSize);
   return (
     <div className="flex h-full flex-col items-center justify-between overflow-y-auto px-10 py-5">
       <div className="w-full">
-        <div className='flex justify-between'>
+        <div className="flex items-center justify-between">
           <h1 className="py-3 text-xl font-bold">Список публікацій</h1>
-          <NavLink to="/publications/new" className="border border-main-text rounded-full p-3 h-fit font-semibold flex items-center">
-            <img src={PlusIcon} className='max-h-[20px] mr-1 aspect-square inline' />
-            <span>
-              Додати публікацію
-            </span>
-          </NavLink>
+          <div className="flex flex-col gap-3 md:flex-row">
+            <NavLink
+              to="/reports"
+              className="flex h-fit items-center rounded-full border border-main-text p-3 font-semibold"
+            >
+              <img src={ReportIcon} className="mr-1 inline aspect-square max-h-[20px]" />
+              <span>Звіти</span>
+            </NavLink>
+            <NavLink
+              to="/publications/new"
+              className="flex h-fit items-center rounded-full border border-main-text p-3 font-semibold"
+            >
+              <img src={PlusIcon} className="mr-1 inline aspect-square max-h-[20px]" />
+              <span>Додати публікацію</span>
+            </NavLink>
+          </div>
         </div>
         {publicationsList.publications.map((publication) => (
           <PublicationEntry
