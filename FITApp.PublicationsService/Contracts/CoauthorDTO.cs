f8590faa0883b9548e88630b855c35b8ebc18dbe
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-05-19 13:51:26 +0300

    Add contracts

---
+++ FITApp.PublicationsService/Contracts/CoauthorDTO.cs
@@ -0,0 +1,12 @@
+namespace FITApp.PublicationsService.Contracts;
+
+public class CoauthorDTO
+{
+    public string? Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Patronymic { get; set; }
+}
