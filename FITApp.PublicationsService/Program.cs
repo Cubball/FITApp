4f2e1b913274e0dd393c542d4e2d94b6c93e2f2a
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-05-19 16:30:05 +0300

    Add controller mapping

--- FITApp.PublicationsService/Program.cs
+++ FITApp.PublicationsService/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 MongoClient client = new MongoClient(builder.Configuration["MongoSettings:ConnectionString"]);
@@ -29,5 +30,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.MapControllers();
 app.Run();
