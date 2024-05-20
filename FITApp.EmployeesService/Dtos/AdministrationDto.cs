5abd9b741a32b12a1e4fdf32c5ad93ff1c6a91ff
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-05-20 14:35:38 +0300

    Add dtos

---
+++ FITApp.EmployeesService/Dtos/AdministrationDto.cs
@@ -0,0 +1,9 @@
+namespace FITApp.EmployeesService.Dtos
+{
+    public class AdministrationDto
+    {
+        public AuthorDto HeadOfDepartment { get; set; }
+
+        public AuthorDto ScientificSecretary { get; set; }
+    }
+}
