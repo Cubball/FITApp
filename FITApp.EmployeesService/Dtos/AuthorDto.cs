b266139eec4d128ec2d28ec853853818dc5d31df
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-05-20 14:35:38 +0300

    Add dtos

---
+++ FITApp.EmployeesService/Dtos/AuthorDto.cs
@@ -0,0 +1,13 @@
+namespace FITApp.EmployeesService.Dtos
+{
+    public class AuthorDto
+    {
+        public string? Id { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Patronymic { get; set; } = null!;
+    }
+}
